dfab21ddddc373b048438e8341160a2f001aff29
thanhnguyen97 <thanhnguyenxh97@gmail.com>
2019-01-11 19:33:26 +0700

    updated login

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 // import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component{
+
+    state = { loggedIn: null };
 
     initializeFirebase() {
         const firebase = require("firebase");
@@ -25,12 +27,31 @@ class App extends Component{
       
       componentWillMount() {
         this.initializeFirebase();
-      
+        
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user) {
+                this.setState({loggedIn: true });
+            } else {
+                this.setState({loggedIn: false });
+            }
+        });
       }
+
+    renderContent() { 
+        switch (this.state.loggedIn) {
+            case true:
+                return <Button>Log out</Button>
+            case false: 
+                return <LoginForm/>
+            default: 
+                return <Spinner size="large" />;
+        }
+    }
     render(){
         return(
             <View>
                 <Header headerText="Authentication"/>
+                {this.renderContent()}
                 <LoginForm />
             </View>
 
